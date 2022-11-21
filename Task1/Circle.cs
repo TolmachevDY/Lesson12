d55e626fca981f4fb925dd1cc923a934efe3159f
Tolmachev DY <Tolmachev DY@DESKTOP-5RJHKAS>
2022-11-21 23:35:38 +0300

    Добавьте файлы проекта.

---
+++ Task1/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    static class Circle
+    {
+        static public double GetLenght(double r)
+        {
+            return 2.0*Math.PI*r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI*r*r;
+        } 
+        static public double GetPoint(double ox, double oy, double mx, double my)
+        {
+            return Math.Sqrt(Math.Pow(ox-mx,2)+Math.Pow(oy-my,2));            
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга");
+            double r = Convert.ToDouble(Console.ReadLine());           
+            double lenght=Circle.GetLenght(r);
+            double square=Circle.GetSquare(r);
+            Console.WriteLine($"Длина окружности равна: {lenght}\nПлощадь круга равна: {square}");
+            Console.Write("Введите координаты центра круга:\nxО=");
+            double ox = Convert.ToDouble(Console.ReadLine());
+            Console.Write("yО=");
+            double oy = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координаты точки:\nx=");
+            double mx = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y=");
+            double my = Convert.ToDouble(Console.ReadLine());
+            double point = Circle.GetPoint(ox, oy, mx, my);
+            if (point <= r)
+                Console.WriteLine("Точка принадлежит кругу!");
+            else
+                Console.WriteLine("Точка не принадлежит кругу!");
+            Console.ReadKey();
+        }
+    }
+}
